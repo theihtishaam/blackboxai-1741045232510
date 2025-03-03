283c74572986d21c8e3e0bcfcd374bf8cf2732bf
theihtishaam <59793939+theihtishaam@users.noreply.github.com>
2025-03-04 03:41:16 +0400

    Add mobile-app/src/constants/icons.js 1741045275512

---
+++ mobile-app/src/constants/icons.js
@@ -0,0 +1,75 @@
+// Tab Icons
+export const TAB_ICONS = {
+  camera: {
+    active: require('../../assets/icons/camera-active.png'),
+    inactive: require('../../assets/icons/camera-inactive.png'),
+  },
+  stories: {
+    active: require('../../assets/icons/stories-active.png'),
+    inactive: require('../../assets/icons/stories-inactive.png'),
+  },
+  profile: {
+    active: require('../../assets/icons/profile-active.png'),
+    inactive: require('../../assets/icons/profile-inactive.png'),
+  },
+};
+
+// Auth Icons
+export const AUTH_ICONS = {
+  google: require('../../assets/icons/google.png'),
+  apple: require('../../assets/icons/apple.png'),
+  email: require('../../assets/icons/email.png'),
+  password: require('../../assets/icons/password.png'),
+  eye: require('../../assets/icons/eye.png'),
+  eyeOff: require('../../assets/icons/eye-off.png'),
+};
+
+// Camera Icons
+export const CAMERA_ICONS = {
+  flip: require('../../assets/icons/flip.png'),
+  flash: require('../../assets/icons/flash.png'),
+  flashOff: require('../../assets/icons/flash-off.png'),
+  capture: require('../../assets/icons/capture.png'),
+  filter: require('../../assets/icons/filter.png'),
+  gallery: require('../../assets/icons/gallery.png'),
+};
+
+// Story Icons
+export const STORY_ICONS = {
+  add: require('../../assets/icons/add.png'),
+  like: require('../../assets/icons/like.png'),
+  liked: require('../../assets/icons/liked.png'),
+  comment: require('../../assets/icons/comment.png'),
+  share: require('../../assets/icons/share.png'),
+  more: require('../../assets/icons/more.png'),
+};
+
+// Profile Icons
+export const PROFILE_ICONS = {
+  edit: require('../../assets/icons/edit.png'),
+  settings: require('../../assets/icons/settings.png'),
+  subscription: require('../../assets/icons/subscription.png'),
+  help: require('../../assets/icons/help.png'),
+  logout: require('../../assets/icons/logout.png'),
+};
+
+// Common Icons
+export const COMMON_ICONS = {
+  back: require('../../assets/icons/back.png'),
+  close: require('../../assets/icons/close.png'),
+  check: require('../../assets/icons/check.png'),
+  error: require('../../assets/icons/error.png'),
+  warning: require('../../assets/icons/warning.png'),
+  info: require('../../assets/icons/info.png'),
+  search: require('../../assets/icons/search.png'),
+  menu: require('../../assets/icons/menu.png'),
+};
+
+export default {
+  TAB_ICONS,
+  AUTH_ICONS,
+  CAMERA_ICONS,
+  STORY_ICONS,
+  PROFILE_ICONS,
+  COMMON_ICONS,
+};
